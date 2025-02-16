b81c0137426adac1fbb97cc59bdc4d893625ea54
jaeeun-yun <a40529120@gmail.com>
2025-02-16 14:44:40 +0900

    문제풀이 추가

---
+++ Lectures/Day-6/answer6.py
@@ -0,0 +1,27 @@
+"""
+# question1
+def gugudan(num):
+    for i in range(1, 10):
+        print(f"{num} * {i} = {num * i}")
+for i in range(1,10):
+    gugudan(i)
+"""
+
+
+"""
+# question2
+# 두 float 타입의 숫자를 더해서, 반올림된 정수를 반환하는 함수
+def add(a:float, b:float)->int:
+    return round(a + b)
+"""
+
+"""
+# question3
+data = [1, 2, 3, 4, 5]
+
+def double(x):
+    return x * 2
+
+result = [double(num) for num in data ]
+"""
+
