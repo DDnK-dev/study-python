1646ff378a9ff98adeebc52b0f3d7a50ec295989
jaeeun-yun <a40529120@gmail.com>
2025-02-27 21:10:49 +0900

    문제풀이 추가

--- Lectures/Day-7/quiz/quiz.py
+++ Lectures/Day-7/quiz/quiz.py
@@ -5,7 +5,21 @@ Quiz 1.
 이때, 함수의 이름은 function_name, 함수의 호출 시간은 elapsed_time으로 출력하시오.
 데코레이터의 이름은 timer로 하시오.
 """
+import functools
+import time
 
+
+def timer(function):
+    @functools.wraps(function)
+    def put_function(n):
+        print(f'{put_function.__name__}')
+        start_time = time.time()
+        function(n)
+        elapsed_time = time.time() - start_time
+        return elapsed_time
+    return put_function
+
+@ timer
 def fibonacci(n):
     if n < 2:
       return n
